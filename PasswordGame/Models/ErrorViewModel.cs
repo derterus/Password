990be7ece735c8834f64592e31003fb8bc479c99
Admiral271 <ienikesergey@gmail.com>
2023-11-02 20:37:07 +0800

    Initial

---
+++ PasswordGame/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PasswordGame.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

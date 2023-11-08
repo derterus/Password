d3a6d15e7989a006023222b95b6d8f509ff83a2a
derterus <nikitarabcev69@gmail.com>
2023-11-08 13:40:29 +0800

    add rules

--- PasswordGame/wwwroot/js/site.js
+++ PasswordGame/wwwroot/js/site.js
@@ -5,7 +5,8 @@
 var app = new Vue({
     el: '#app',
     data: {
-        index: 2,
+        index: 1,
+        hash=true,
         password: "",
         rulesStatus: [
             { id: 1, statusNow: false, statusComplete: false, text: "Ваш пароль должен иметь по крайней мере 5 символов"},
@@ -15,9 +16,32 @@ var app = new Vue({
 
         ]
     },
-    computed:{
-        checkedPassword: function (rule) {
-            rule.text="123123123"
+    computed: {
+        filterRules() {
+            return this.rulesStatus.filter(rule=>rule.statusComplete===true)
+        },
+        filterIndex() {
+            return this.rulesStatus.filter(rule => rule.id === this.index)
+        }
+    },
+    methods:{
+        checkedPassword: function () {
+            if (this.password.length > 5) {
+                this.rulesStatus[0].statusComplete = true
+                this.rulesStatus[0].statusNow = true
+                if (this.index == 1) { this.index++ }
+            }
+            else {
+                this.rulesStatus[0].statusNow = false
+            }
+          // сделать проверку  if (/\d/.test(this.password)=true) {
+                this.rulesStatus[1].statusComplete = true
+                this.rulesStatus[1].statusNow = true
+                if (this.index == 2) { this.index++ }
+            }
+            else {
+                this.rulesStatus[0].statusNow = false
+            }
         }
     }
 })

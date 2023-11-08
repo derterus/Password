8f61cac19c2f1a1db295825104c01ae4e9128a43
derter <nikitarabcev69@gmail.com>
2023-11-08 19:34:42 +0800

    join second rule

--- PasswordGame/wwwroot/js/site.js
+++ PasswordGame/wwwroot/js/site.js
@@ -1,19 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿
 var app = new Vue({
     el: '#app',
     data: {
         index: 1,
-        hash=true,
         password: "",
         rulesStatus: [
             { id: 1, statusNow: false, statusComplete: false, text: "Ваш пароль должен иметь по крайней мере 5 символов"},
             { id: 2, statusNow: false, statusComplete: false, text: "Ваш пароль должен иметь по крайней мере 1 цифру" }
-
-
-
         ]
     },
     computed: {
@@ -25,7 +18,8 @@ var app = new Vue({
         }
     },
     methods:{
-        checkedPassword: function () {
+        checkedPassword: function ()
+        {
             if (this.password.length > 5) {
                 this.rulesStatus[0].statusComplete = true
                 this.rulesStatus[0].statusNow = true
@@ -34,13 +28,13 @@ var app = new Vue({
             else {
                 this.rulesStatus[0].statusNow = false
             }
-          // сделать проверку  if (/\d/.test(this.password)=true) {
+           if (/\d/.test(this.password)==true) {
                 this.rulesStatus[1].statusComplete = true
                 this.rulesStatus[1].statusNow = true
                 if (this.index == 2) { this.index++ }
             }
             else {
-                this.rulesStatus[0].statusNow = false
+                this.rulesStatus[1].statusNow = false
             }
         }
     }

1230f21be1e8ca14a8530c0bceb33d025dd71b89
derterus <nikitarabcev69@gmail.com>
2023-11-09 14:42:49 +0800

    add multiply rule

--- PasswordGame/wwwroot/js/site.js
+++ PasswordGame/wwwroot/js/site.js
@@ -9,6 +9,8 @@ var app = new Vue({
             { id: 2, statusNow: false, statusComplete: false, text: "Ваш пароль должен иметь по крайней мере 1 цифру" },
             { id: 3, statusNow: false, statusComplete: false, text: "Ваш пароль должен иметь по крайней мере 10 символов" },
             { id: 4, statusNow: false, statusComplete: false, text: "Ваш пароль должен иметь по крайней мере 15 символов" },
+            { id: 5, statusNow: false, statusComplete: false, text: "Ваш пароль должен числа с общим перемножением равным 25" },
+
         ]
     },
     computed: {
@@ -22,7 +24,7 @@ var app = new Vue({
     methods: {
         checkedPassword: function () {
             //1 rule
-            if (this.password.length > 5) {
+            if (this.password.length >= 5) {
                 this.rulesStatus[0].statusComplete = true
                 this.rulesStatus[0].statusNow = true
                 if (this.index == 1) { this.index++ }
@@ -41,7 +43,7 @@ var app = new Vue({
             }
 
             //3 rule
-            if (this.password.length > 10) {
+            if (this.password.length >= 10) {
                 this.rulesStatus[2].statusComplete = true
                 this.rulesStatus[2].statusNow = true
                 if (this.index == 3) { this.index++ }
@@ -50,7 +52,7 @@ var app = new Vue({
                 this.rulesStatus[2].statusNow = false
             }
             //4 rule
-            if (this.password.length > 15) {
+            if (this.password.length >= 15) {
                 this.rulesStatus[3].statusComplete = true
                 this.rulesStatus[3].statusNow = true
                 if (this.index == 4) { this.index++ }
@@ -58,6 +60,26 @@ var app = new Vue({
             else {
                 this.rulesStatus[3].statusNow = false
             }
+
+            //5 rule
+
+            var multi = String(parseInt(this.password.match(/\d+/)))
+            var sum=1
+            for (var i = 0; i<multi.length;i++) {
+                sum *= parseInt(multi[i])
+            }
+            if (sum==25) 
+            {
+                this.rulesStatus[4].statusComplete = true
+                this.rulesStatus[4].statusNow = true
+                if (this.index == 5) { this.index++ }
+            }
+            else {
+                this.rulesStatus[4].statusNow = false
+            }
+             //6 rule
+
+
         }
     }
 })
